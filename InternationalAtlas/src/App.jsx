ece55b8bc3cbbd1af25acb1954862fdea3819306
Ajinkya-Chitale <ajinkyachitale89@gmail.com>
2025-02-27 14:02:36 +0530

    Done with initial setup (create pages, routing, layout, outlet etc.)

--- InternationalAtlas/src/App.jsx
+++ InternationalAtlas/src/App.jsx
@@ -1,10 +1,44 @@
 import './App.css'
+import { createBrowserRouter, RouterProvider } from 'react-router'
+import Layout from './UI/Layout/Layout'
+import Home from './UI/pages/Home'
+import About from './UI/pages/About'
+import Country from './UI/pages/Country'
+import Contact from './UI/pages/Contact'
+import NotFound from './UI/pages/NotFound'
 
 function App() {
+  const router = createBrowserRouter([
+    {
+      path: '/',
+      element: <Layout />,
+      errorElement: <NotFound />,
+
+      children: [
+        {
+          path: '/',
+          element: <Home />
+        },
+        {
+          path: '/about',
+          element: <About />
+        },
+        {
+          path: '/country',
+          element: <Country />
+        },
+        {
+          path: '/contact',
+          element: <Contact />
+        }
+      ]
+    }
+  ])
+
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <>
+      <RouterProvider router={router} />
+    </>
   )
 }
 
